23485fb12f190b1ec8185778776d6bd197bd4273
Chandler J <cjustice2000@gmail.com>
2024-08-28 22:42:23 -0600

    formatted strings plus cleaner code

--- src/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=native=/usr/lib/libX11.so.6.4.0");
-    println!("cargo:rustc-link-lib=x11");
-}
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ use std::time::Duration;
 use x11rb::connection::Connection;
 use x11rb::protocol::xproto::*;
 use x11rb::rust_connection::RustConnection;
-use x11rb::protocol::xproto::Atom;
+
 
 
 fn main() {
-//   println!("cargo::rustc-link-lib=/use/X11R6/lib");
+
    let mut sys = System::new_all(); 
    let (conn, screen_num) = RustConnection::connect(None).unwrap();
    let screen = &conn.setup().roots[screen_num];
@@ -27,7 +27,7 @@ fn main() {
        let memory = get_memory(&mut sys);
        let time = get_time(&mut sys, "America/Denver");
        
-       let output = match CString::new(format!("cpu: {}%  mem: {}%  {}", cpu_usage, memory, time)) {
+       let output = match CString::new(format!("cpu: {:2}%  mem: {:2}%  {}", cpu_usage, memory, time)) {
 
             Ok(out) => out,
             Err(e) => {
@@ -63,7 +63,7 @@ fn get_time(sys: &mut sysinfo::System, timezone: &str) -> String {
        Ok(tz) => {
             let utc: DateTime<Utc> = Utc::now();
             let local: DateTime<Tz> = utc.with_timezone(&tz);
-            local.format("%H:%M:%S").to_string()
+            local.format("%H:%M:%S %m/%d").to_string()
        }
        Err(e) => {
             e.to_string()
@@ -75,7 +75,6 @@ fn get_time(sys: &mut sysinfo::System, timezone: &str) -> String {
 fn get_memory(sys: &mut sysinfo::System) -> u64 {
     sys.refresh_memory_specifics(MemoryRefreshKind::new().with_ram());
     if sys.total_memory() == 0 {
-        println!("REACHED");
         return 0
     }
     ((sys.used_memory() as f64/sys.total_memory() as f64) * 100.0) as u64
@@ -83,27 +82,14 @@ fn get_memory(sys: &mut sysinfo::System) -> u64 {
 
 
 fn get_cpu_usage(sys: &mut sysinfo::System) -> u32{
-       sys.refresh_cpu_usage();
-       let mut totalusage: f32 = 0.0;
-       for cpu in sys.cpus(){
-           totalusage += cpu.cpu_usage();
-       } 
-       (totalusage / sys.cpus().len() as f32) as u32
+    sys.refresh_cpu_usage();
+    let mut totalusage: f32 = 0.0;
+    for cpu in sys.cpus(){
+        totalusage += cpu.cpu_usage();
+    } 
+    (totalusage / sys.cpus().len() as f32) as u32
 }
 
-fn parse_temp_file () {
-    ()
-}
 
-fn get_system_temps() {
-    let path = "/sys/class/thermal/";
-    
-    match fs::read_dir(path) {
-        Ok (entries) => {
 
-        }
-        Err (e) => {
 
-        }
-    }
-}
